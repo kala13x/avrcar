36987be26e9a4afba06440e1c9bad1e9ca3a9d40
abcdach <d.chkhaidze@gmail.com>
2013-06-13 13:40:33 +0400

    mimoxedva

--- Tasks/Task_06.c
+++ Tasks/Task_06.c
@@ -11,6 +11,9 @@ unsigned short b=0;
 unsigned char Servo_Pos_Step     = 0;
 unsigned short Servo_Motor_Delay = 0;
 unsigned char  Servo_Motor_Dir   = 0;
+unsigned char Servo_Pos          = 0;
+unsigned long Gamura_Vel[2];
+
 
 void Task_06(void)
 {
@@ -36,16 +39,19 @@ void Task_06(void)
 					case 0 :
 					{
 						Servo_L_45;
+						Servo_Pos=0;
 						break;
 					}
 					case 1 :
 					{
 						Servo_Centre;
+						Servo_Pos=1;
 						break;
 					}
 					case 2 :
 					{
 						Servo_R_45;
+						Servo_Pos=2;
 						break;
 					}
 				}
@@ -94,7 +100,7 @@ void Task_06(void)
 			{
 				Servo_Motor_Delay++;
 
-				if(Servo_Motor_Delay == 50000)
+				if(Servo_Motor_Delay == 25000)
 				{
 
 					Task_06__GoTo_Next_Step	;
@@ -108,35 +114,36 @@ void Task_06(void)
 			}
 			case 3 :
 			{
-				Task_06__Task_Start	;
+				Task_02__Task_Start;
+
+
+				Task_06__GoTo_Next_Step	;
 				break;
 			}
 			case 4 :
 			{
-				b++;
-
-				if(b  == 60000)
+				if(Task_02_Step  == 0)
 				{
-
-					Task_06__Task_Start	;
-
+					Gamura_Vel[Servo_Pos]=MyVel._long;
+					Task_06__GoTo_Next_Step	;
 				}
-
-
 				break;
 			}
 
-			/*case 5 :
+			case 5 :
 			{
+				USART2_Waiting_For_Free ;
+				sprintf(USART2_buffer,"%lu  %lu  %lu\n\r", Gamura_Vel[0], Gamura_Vel[1], Gamura_Vel[2]);
+				USART2_Transmit();
+				Task_06__Task_Start;
 
-				Task_03__Task_Stop;
 				break;
 			}
 
 
 
 
-		}*/
+
 	}
 
 
